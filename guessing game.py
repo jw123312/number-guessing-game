7ce9260b6996a1f6a0809ea120e98cb6fd55c4c3
jw123312 <jianwei123312@gmail.com>
2022-07-21 11:29:29 +0800

    done

--- guessing game.py
+++ guessing game.py
@@ -1,1 +1,27 @@
- 
+import random
+
+won = True
+
+while True:
+
+    if won:    
+        num = random.randint(1,100)
+        won = False
+
+    #ensure that number wont be blank
+    while (inp := input("Number to guess:")) == '':
+        pass
+        
+    
+    if inp.lower() == 'q':
+        break;
+
+    inp = int(inp)
+    
+    if inp == num:
+        print("you won!")
+        won = True
+    elif inp > num:
+        print("Your number is greater")
+    else:
+        print("Your number is lower")
